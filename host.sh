9024a18a8b6a05fdf1ce6088850b4466413504d9
Ryan Birmingham <birm@rbirm.us>
2018-02-02 12:45:40 -0500

    fix wrong hostname

--- host.sh
+++ host.sh
@@ -1,7 +1,7 @@
-host_url=$1
+hub_url=$1
 hub_pw=$2
 hostname=$3
 
 curl -X POST "$hub_url/post/services" --data "{admin_password: $hub_pw, host = $hostname}"
 
-nodejs index.js $hostname
+nodejs index.js $hub_url
