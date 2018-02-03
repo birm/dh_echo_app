35221fc5f501704a7aebec7a06c91079789ef260
Ryan Birmingham <birm@rbirm.us>
2018-02-03 03:22:37 -0500

    fix service reg

--- host.sh
+++ host.sh
@@ -3,6 +3,6 @@ hub_pw=$2
 hostname=$3
 
 bash wait_for.sh $hub_url
-curl -X POST "$hub_url/post/services" --data "{admin_password: $hub_pw, host:$hostname}"
+curl -X POST "$hub_url/post/services" --data "{admin_password: $hub_pw, service:echo host:$hostname}"
 
 nodejs index.js $hub_url
